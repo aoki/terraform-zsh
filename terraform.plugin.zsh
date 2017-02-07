49a219a0d3d242e7c4c0b6a8f2ed2e2264f82fd9
Yoshiki Aoki <ringohub@gmail.com>
2017-02-08 02:07:16 +0900

    update

--- terraform.plugin.zsh
+++ terraform.plugin.zsh
@@ -1,4 +1,5 @@
-source completions/_terraform
+# source completions/_terraform
 alias tf='terraform'
-compdef terraform
-compdef tf=terraform
+# compdef terraform
+# compdef tf=terraform
+fpath+="$(cd $(dirname $0) && pwd)/completions"
